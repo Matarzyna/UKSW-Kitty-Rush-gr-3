019ec361e07d9303db652b2daa5891ef9b6ffd77
MarcinPowalka <153096728+MarcinPowalka@users.noreply.github.com>
2024-12-04 12:43:42 +0100

    Dodanie animacji do niszczenia krzaczków

--- scripts/bush.gd
+++ scripts/bush.gd
@@ -1,4 +1,21 @@
 extends StaticBody2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@export var move_time = 0.3
 
+var is_destroy = false
+var move_timer = 0.0 
+
+func _physics_process(delta):
+	if is_destroy:
+		move_timer += delta
+		var progress = move_timer / move_time
+		if progress >= 1.0:
+			progress = 1.0
+			is_destroy = false
+			queue_free()
+			
+			
 func body_entered():
-	queue_free()  # Usuwa bieżący węzeł
+	animated_sprite_2d.play("destroy")
+	is_destroy = true
+	move_timer = 0.0
--- scripts/studentka.gd
+++ scripts/studentka.gd
@@ -1,12 +1,10 @@
-
-
 extends CharacterBody2D
 @export var tile_size = 16  # Rozmiar kafelka (w pikselach)
 @export var move_time = 0.3  # Czas ruchu w sekundach
 @onready var animated_sprite_2d = $AnimatedSprite2D  # Ścieżka do AnimatedSprite2D
 @onready var ray = $RayCast2D
 
-var face_direction = "Right"
+var face_direction = "right"
 var is_moving = false  # Flaga kontrolująca, czy postać obecnie się porusza
 var move_direction = Vector2.ZERO  # Kierunek ruchu
 var move_timer = 0.0  # Licznik czasu dla ruchu
@@ -40,54 +38,58 @@ func handle_input():
 		ray.set_target_position(Vector2.UP * 16)
 		ray.force_raycast_update()
 		if !ray.is_colliding():
-			if(face_direction == "Right"):
+			if(face_direction == "right"):
 				start_movement(Vector2.UP, "clime_r")
 			else:
 				start_movement(Vector2.UP, "clime_l")
 		else:
-			var collider = ray.get_collider()
-			if collider.is_in_group('bushs'):
-				collider.body_entered()
+			start_attack(face_direction)
+	
 	elif Input.is_action_pressed("ui_down"):
 		ray.set_target_position(Vector2.DOWN * 16)
 		ray.force_raycast_update()
 		if !ray.is_colliding():
-			if(face_direction == "Right"):
+			if(face_direction == "right"):
 				start_movement(Vector2.DOWN, "clime_r")
 			else:
 				start_movement(Vector2.DOWN, "clime_l")
 		else:
-			var collider = ray.get_collider()
-			if collider.is_in_group('bushs'):
-				collider.body_entered()
+			start_attack(face_direction)
+	
 	elif Input.is_action_pressed("ui_left"):
 		ray.set_target_position(Vector2.LEFT * 16)
 		ray.force_raycast_update()
+		face_direction = "left"
 		if !ray.is_colliding():
-			face_direction = "Left"
 			start_movement(Vector2.LEFT, "walk_l")
 		else:
-			var collider = ray.get_collider()
-			if collider.is_in_group('bushs'):
-				collider.body_entered()
+			start_attack(face_direction)
+	
 	elif Input.is_action_pressed("ui_right"):
 		ray.set_target_position(Vector2.RIGHT * 16)
 		ray.force_raycast_update()
+		face_direction = "right"
 		if !ray.is_colliding():
-			face_direction = "Right"
 			start_movement(Vector2.RIGHT, "walk_r")
 		else:
-			var collider = ray.get_collider()
-			if collider.is_in_group('bushs'):
-				animated_sprite_2d.play("niszcz")
-				is_destroy = true
-				move_timer = 0.0
-				collider.body_entered()
+			start_attack(face_direction)
+	
 	else:
-		if(face_direction == "Right"):
+		if (face_direction == "right"):
 			animated_sprite_2d.play("stay_r")
 		else:
 			animated_sprite_2d.play("stay_l")
+	
+func start_attack(direction: String):
+	var collider = ray.get_collider()
+	if collider.is_in_group('bushs'):
+		if(direction == "left"):
+			animated_sprite_2d.play("destroy_l")
+		else:
+			animated_sprite_2d.play("destroy_r")
+		is_destroy = true
+		move_timer = 0.0
+		collider.body_entered()
 
 func start_movement(direction: Vector2, animation: String):
 	is_moving = true
