717eceb46b46a834939a7edac0acc8a2f5c72dae
jaagoo783 <jagodaj755@gmail.com>
2024-12-19 17:04:21 +0100

    poszerzenie menu o dodatkowe opcje

--- menu.gd
+++ menu.gd
@@ -5,10 +5,14 @@ func _on_start_pressed() -> void:
 	print("Start pressed")
 	get_tree().change_scene_to_file("res://Mapa_0.tscn")
 
-
+func _on_continue_pressed() -> void:
+	print("Continue pressed")
+	
 func _on_settings_pressed() -> void:
 	print("Settings pressed")
 
+func _on_credits_pressed() -> void:
+	print("Credits pressed")
 
 func _on_exit_pressed() -> void:
 	print("Exit pressed")
