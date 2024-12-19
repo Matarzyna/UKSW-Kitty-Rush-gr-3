6829ae4b638e26b907cf30c7d2934ae4d41dc552
jaagoo783 <jagodaj755@gmail.com>
2024-12-19 18:29:01 +0100

    dodanie licznika kotkow

---
+++ game_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var cats_score = 0
+
+func add_cat_count() -> void:
+	cats_score += 1
+	print("Cats: ", cats_score)
--- scripts/kotek.gd
+++ scripts/kotek.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
 
 func _on_body_entered(body: Node2D) -> void:
-	print("+1 kotek")
+	game_manager.add_cat_count()  
 	queue_free()
