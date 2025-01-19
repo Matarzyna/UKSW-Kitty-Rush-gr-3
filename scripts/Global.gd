59c853a2d96b8f5600a68ae0f60aa307a48fd31c
Matarzyna <kasia.mich78@gmail.com>
2025-01-19 17:24:14 +0100

    Dodano grubego do UI, animacja działa poprawnie

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -3,7 +3,10 @@ extends Node
 var current_scene_state = null
 var paused: bool = false
 var pause_menu = null
-var cat_counter = 0
+
+var cat_counter = 0  # Licznik zwykłych kotków
+var gruby_cat_counter = 0  # Licznik grubych kotków
+
 var life = 5
 var globalLife = 5
 var death_position = Vector2(-104,80)
@@ -11,6 +14,7 @@ var current_position = Vector2(-104,80)
 
 var bushes_state = {}  # Przechowuje stany krzaków
 var cats_state = {}  # Przechowuje stany kotków
+var gruby_cats_state = {}  # Przechowuje stany grubych kotków
 var chests_state = {}  # Przechowuje stany skrzynek
 
 func set_paused(is_paused):
@@ -18,6 +22,7 @@ func set_paused(is_paused):
 
 func reset_kitty_counter():
 	cat_counter = 0
+	gruby_cat_counter = 0
 		
 func reset_life():
 	globalLife = 5
@@ -29,6 +34,10 @@ func reset_position():
 func reset_cats_state():
 	for cat in cats_state:
 		cats_state[cat] = {"is_collected": false, "visible": true}
+
+func reset_gruby_cats_state():
+	for gruby_cat in gruby_cats_state:
+		gruby_cats_state[gruby_cat] = {"is_collected": false, "visible": true}
 
 func reset_bushes_state():
 	for bush in bushes_state:
@@ -43,5 +52,6 @@ func reset_game():
 	reset_life()
 	reset_position()
 	reset_cats_state()
+	reset_gruby_cats_state()
 	reset_bushes_state()
 	reset_chests_state()
--- scripts/save_manager.gd
+++ scripts/save_manager.gd
@@ -8,13 +8,14 @@ func save_game():
 	var save_data = {
 		"bushes_state": Global.bushes_state,
 		"cats_state": Global.cats_state,
+		"gruby_cats_state": Global.gruby_cats_state,
 		"chests_state": Global.chests_state,
 		"player_position": Global.death_position,
 		"GlobalLife": Global.globalLife,
 		"life": Global.life,
 		"cat_counter": Global.cat_counter
 	}
-	var file = FileAccess.open("user://save_game.dat", FileAccess.WRITE)
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
 	if file:
 		file.store_var(save_data)
 		file.close()
@@ -22,7 +23,11 @@ func save_game():
 
 # Funkcja wczytywania gry
 func load_game():
-	var file = FileAccess.open("user://save_game.dat", FileAccess.READ)
+	if not FileAccess.file_exists(SAVE_FILE_PATH):
+		print("Brak pliku zapisu, gra nie została wczytana")
+		return
+
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
 	if file:
 		var save_data = file.get_var()
 		file.close()
@@ -31,7 +36,9 @@ func load_game():
 		Global.bushes_state = save_data.get("bushes_state", {})
 		print("Stany krzaków załadowane")
 		Global.cats_state = save_data.get("cats_state", {})
-		print("Stany kotów załadowane")
+		print("Stany kotków załadowane")
+		Global.gruby_cats_state = save_data.get("gruby_cats_state", {})
+		print("Stany grubych kotków załadowane")
 		Global.chests_state = save_data.get("chests_state", {})
 		print("Stany skrzynek załadowane")
 
@@ -47,27 +54,41 @@ func save_state():
 	var bushes = get_tree().get_nodes_in_group("bushes")
 	Global.bushes_state.clear()
 	for bush in bushes:
-		Global.bushes_state[bush.name] = {
-			"is_destroyed": bush.is_destroy,
-			"visible": bush.visible
-		}
+		if "is_destroy" in bush and "visible" in bush:
+			Global.bushes_state[bush.name] = {
+				"is_destroyed": bush.is_destroy,
+				"visible": bush.visible
+			}
 	print("Stany krzaków zapisane")
 
 	# Zapisywanie stanów kotków
 	var cats = get_tree().get_nodes_in_group("cats")
 	Global.cats_state.clear()
 	for cat in cats:
-		Global.cats_state[cat.name] = {
-			"is_collected": cat.is_collected,
-			"visible": cat.visible
-		}
+		if "is_collected" in cat and "visible" in cat:
+			Global.cats_state[cat.name] = {
+				"is_collected": cat.is_collected,
+				"visible": cat.visible
+			}
 	print("Stany kotków zapisane")
+
+	# Zapisywanie stanów grubych kotków
+	var Fatcats = get_tree().get_nodes_in_group("FatCats")
+	Global.gruby_cats_state.clear()
+	for F in Fatcats:
+		if "is_collected" in F and "visible" in F:
+			Global.gruby_cats_state[F.name] = {
+				"is_collected": F.is_collected,
+				"visible": F.visible
+			}
+	print("Stany grubych kotków zapisane")
 	
 	# Zapisywanie stanów skrzynek
 	var chests = get_tree().get_nodes_in_group("chests")
 	Global.chests_state.clear()
 	for chest in chests:
-		Global.chests_state[chest.name] = {
-			"is_opened": chest.is_opened,
-		}
-	print("Stany kotków zapisane")
+		if "is_opened" in chest:
+			Global.chests_state[chest.name] = {
+				"is_opened": chest.is_opened
+			}
+	print("Stany skrzynek zapisane")
